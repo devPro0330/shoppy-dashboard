2e062109ecd2c7b5c56c257450b83e64710dc862
thomas <thomastian2041@gmail.com>
2023-06-23 17:49:41 +0800

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { BrowserRouter } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl underline">App</h1>;
+  const activeMenu = true;
+
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="relative flex dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="p-3 text-3xl text-white hover:drop-shadow-xl hover:bg-light-gray"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? <div>Sidebar</div> : <div>Sidebar w-0</div>}
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
