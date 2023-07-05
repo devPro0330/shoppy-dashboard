a3374cf1195047cf0eac1a907ade5670229ffd33
thomas <thomastian2041@gmail.com>
2023-07-05 16:03:50 +0800

    implemented data tables

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Sort,
+  Filter,
+  Page,
+  Selection,
+  Edit,
+  Toolbar,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 function Customers() {
-  return <div>Customers</div>;
+  return (
+    <div className="p-2 m-2 bg-white md:m-10 md:p-10 rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Delete"]}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
 }
 
 export default Customers;
